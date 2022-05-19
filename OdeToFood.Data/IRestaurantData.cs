738fd4189360c90a925851d905721b2840da4174
ron <ron@DESKTOP-P8BO6H9>
2022-05-20 02:57:36 +1000

    figure out bootstrap

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,31 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant(){Id = 1, Name = " Scott's Pizza", Location = "Maryland" },
+                new Restaurant(){Id = 2, Name = " Cinnamon Club", Location = "London" },
+                new Restaurant(){Id = 3, Name = " La Costa", Location = "Califonia" }
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants orderby r.Name select r;
+        }
+    }
+}
--- OdeToFood/Program.cs
+++ OdeToFood/Program.cs
@@ -1,7 +1,11 @@
+using OdeToFood.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+//Ron's adding code
+builder.Services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
 
 var app = builder.Build();
 
